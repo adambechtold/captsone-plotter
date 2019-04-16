f6c4ce32b3b142096219e4dbcd38fc73158217b8
Adam Bechtold <adamcbechtold@gmail.com>
2019-04-16 06:41:32 -0400

    basic nivo fetching

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,66 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import Plot from '../components/plot'
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import Plot from '../components/plot';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <Plot></Plot>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+let myData = [
+  {
+    id: 'group A',
+    data: [
+      {
+        x: 1,
+        y: 1,
+      },
+    ],
+  },
+];
 
-export default IndexPage
+const fetchPosition = () => {
+  const options = {
+    method: 'get',
+    headers: new Headers({'Content-Type': 'application/json'}),
+    // mode: 'no-cors'
+  };
+  return fetch('http://ec2-52-91-195-101.compute-1.amazonaws.com:3000/location', options)
+    .then(response => {
+      console.log(response);
+      return response.json()
+    })
+    .then((responseJson) => {
+      console.log(responseJson);
+      myData[0].data[0].x = responseJson.x;
+      myData[0].data[0].y = responseJson.z;
+      return myData;
+    })
+    .catch(err => console.log('err', err));
+};
+
+class IndexPage extends React.Component {
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      fetchPosition()
+        .then(data => {
+          this.setState({ data });
+        })
+        .catch(err => console.log(err));
+    }, 100);
+  }
+
+  state = {
+    data: myData,
+  };
+
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+        <Plot data={myData} />
+        <Link to="/page-2/">Go to page 2</Link>
+      </Layout>
+    );
+  }
+}
+
+export default IndexPage;
